0cd152a1739b4f7b0de82534879ed69d78747a37
mks110 <55471179+mks110@users.noreply.github.com>
2020-11-10 18:53:46 +0700

    yameteeh

--- ci3/application/controllers/Welcome.php
+++ ci3/application/controllers/Welcome.php
@@ -20,6 +20,18 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('user/index');
+	}
+	public function toko()
+	{
+		$this->load->view('user/toko');
+	}
+	public function login()
+	{
+		$this->load->view('user/login');
+	}
+	public function artikel()
+	{
+		$this->load->view('user/artikel');
 	}
 }
---
+++ ci3/application/views/user/login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <script src="https://kit.fontawesome.com/4f7eb700fd.js" crossorigin="anonymous"></script>
+        <meta charset= "UTF-8">
+        <meta name="viewport" content="width=device-width,initial0scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel = "stylesheet" href="<?php echo base_url() ?>assets/user/css/login.css">
+        <title> Login</title>
+ <body>       
+
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Buat Akun</h1>
+</br>
+			
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+</br>
+			<button>Daftar</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Login</h1>
+</br>
+			
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+</br>
+<button><a href="toko">Masuk</a></button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Sudah Punya Akun?</h1>
+</br>
+</br>
+				<button class="ghost" id="signIn">Login</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Belum Punya Akun?</h1>
+</br>
+</br>
+				<button class="ghost" id="signUp">Daftar</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<script src="<?php echo base_url() ?>assets/user/js/login.js"></script>
+</body>
+</html>
