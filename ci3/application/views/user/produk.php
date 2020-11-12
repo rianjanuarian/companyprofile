3b6e988b1786c3242a40545f529de869dc802ad5
rofiq199 <55471142+rofiq199@users.noreply.github.com>
2020-11-12 08:48:53 +0700

    produk with searchbar

--- ci3/application/views/user/produk.php
+++ ci3/application/views/user/produk.php
@@ -1,11 +1,18 @@
  <link rel="stylesheet" href="<?= base_url('assets/bizpage/') ?>produk.css">
  <section id="produk">
      <div class="container" data-aos="fade-up">
-
-         <header class="section-header">
-             <h3>About Us</h3>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-         </header>
+         <div class="row p-3">
+             <div class="col-12">
+                 <div class="input-group">
+                     <input class="form-control border-secondary py-2" type="search" placeholder="search">
+                     <div class="input-group-append">
+                         <div class="btn btn-outline-success">
+                             <i class="fa fa-search"></i>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
          <div class="row about-cols ">
              <div class="col-md-4 p-3" data-aos="fade-up" data-aos-delay="100">
                  <div class="about-col">
@@ -37,7 +44,7 @@
                      </p>
                      <div class="col">
                          <div class="row">
-                             <button class="form-control col-12 col-md-6" id="show">Detail</button>
+                             <button class="btn btn-outline-success form-control col-12 col-md-6" id="show">Detail</button>
                              <button class="form-control col-12 col-md-6" id="addcart">Add to cart</button>
                          </div>
                      </div>
@@ -56,8 +63,8 @@
                          </p>
                          <div class="col">
                              <div class="row">
-                                 <button class="btn btn-success form-control col-12 col-md-6" id="detail">Detail</button>
-                                 <button class="btn btn-success form-control col-12 col-md-6" id="addcart">Add to cart</button>
+                                 <button class="form-control col-12 col-md-6" style="background-color: #18d26e;color: #fff;" id="detail">Detail</button>
+                                 <button class="form-control col-12 col-md-6" id="addcart">Add to cart</button>
                              </div>
                          </div>
 
