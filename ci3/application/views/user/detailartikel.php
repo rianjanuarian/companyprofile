6976eb33c46d44b5c60c50b856fe93b8a023fa9c
rianjanuarian <55471319+rianjanuarian@users.noreply.github.com>
2020-12-28 10:21:33 +0700

    Update detailartikel.php

--- ci3/application/views/user/detailartikel.php
+++ ci3/application/views/user/detailartikel.php
@@ -1,3 +1,89 @@
+<head>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.containera {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Artikel</title>
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="./assets/css/all.css">
+
+
+    <!-- --------- Owl-Carousel ------------------->
+    <link rel="stylesheet" href="./assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./assets/css/owl.theme.default.min.css">
+
+    <!-- ------------ AOS Library ------------------------- -->
+    <link rel="stylesheet" href="./assets/css/aos.css">
+
+    <!-- Custom Style   -->
+    <link rel="stylesheet" href="./assets/css/Style.css">
+
+</head>
 <!-- Page Content -->
 <section class="detail p-3" style="margin-top:8% ;">
     <div class="container">
@@ -65,3 +151,16 @@
         </div>
     </div>
 </section>
+
+<script src="./assets/js/Jquery3.4.1.min.js"></script>
+
+<!-- --------- Owl-Carousel js ------------------->
+<script src="./assets/js/owl.carousel.min.js"></script>
+
+<!-- ------------ AOS js Library  ------------------------- -->
+<script src="./assets/js/aos.js"></script>
+
+<!-- Custom Javascript file -->
+<script src="./assets/js/main.js"></script>
+    <!-- Bootstrap core JavaScript -->
+ 
