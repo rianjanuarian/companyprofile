6006ffee2f144185af1882bd904e94d15e1467f9
FadhilaDwi <fadhiladwikurniawan@gmail.com>
2020-12-28 12:08:06 +0700

    update sidebar admin

--- ci3/application/views/admin/sidebar.php
+++ ci3/application/views/admin/sidebar.php
@@ -11,7 +11,7 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="<?= base_url('assets/img/profil/') . $admin['foto']; ?>" class="img-circle elevation-2" alt="User Image">
+        <img src="<?= base_url('assets/img/profil/') . $admin['foto']; ?>" class="img-circle elevation-2" >
       </div>
       <div class="info">
         <a href="<?= base_url('admin/Profil') ?>" class="d-block"><?php echo $this->session->userdata('username'); ?></a>
