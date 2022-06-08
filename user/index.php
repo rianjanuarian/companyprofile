5318309b8eef9dfc950812fe318f90bc8684230f
Mohammad Rizki Yanuarianto <reeyanrian@gmail.com>
2022-06-08 12:23:49 +0700

    carousel

--- user/index.php
+++ user/index.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width,initial0scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel = "stylesheet" href="index.css">
+        <link rel = "stylesheet" href="carousel.css">
         <title> Alfa Tech Media</title>
 </head>
 <body>
@@ -32,6 +33,37 @@
     </div>
 </section>
 
+
+<section class="about bg-light">
+
+<div class="container1">
+  <div class="card-carousel">
+    <div class="card" id="1">
+      <div class="image-container"></div>
+      <p>VGA Terbaik memberikan render terbaik untuk game anda</p>
+    </div>
+    <div class="card" id="2">
+      <div class="image-container"></div>
+      <p>Processor terbaik memberikan pengalaman terbaik dalam game anda</p>
+    </div>
+    <div class="card" id="3">
+      <div class="image-container"></div>
+      <p>Motherboard terbaik memberikan performa terbaik dari tiap komponen pc anda</p>
+    </div>  
+    <div class="card" id="4">
+      <div class="image-container"></div>
+      <p>Power Supply terbaik akan menjaga umur pemakaian komponen pc anda</p>
+    </div>
+    <div class="card" id="5">
+      <div class="image-container"></div>
+      <p>RAM terbaik akan memberikan pengalaman multitasking tanpa hambatan</p>
+    </div>
+  </div>
+  <a href="#" class="visuallyhidden card-controller">Carousel controller</a>
+</div>
+<!-- 
+<h2>Check out <a target="_blank" href="https://codepen.io/WillyW/pen/RwRrvjv">Circle Carousel</a></h2> -->
+</section>
 <section class="about bg-light">
     <div class="container">
         <div class="grid-2">
@@ -52,4 +84,5 @@
 
 
 </body>
+<script type="text/javascript" src="carousel.js"></script>
 </html>
